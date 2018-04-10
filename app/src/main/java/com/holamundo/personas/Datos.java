534be4ef7827617b463b563214946f3196b2794d
Sorelyth <sore.vp@gmail.com>
2018-04-09 21:05:40 -0500

    Aplicacion terminada sin validaciones

---
+++ app/src/main/java/com/holamundo/personas/Datos.java
@@ -0,0 +1,22 @@
+package com.holamundo.personas;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Sore on 09/04/2018.
+ */
+
+public class Datos {
+    private static ArrayList<Persona> personas = new ArrayList<>();
+
+    public static void guardar(Persona p){
+        personas.add(p);
+    }
+
+    public static ArrayList<Persona> obtener(){
+        return personas;
+    }
+
+
+}
+
--- app/src/main/java/com/holamundo/personas/Main.java
+++ app/src/main/java/com/holamundo/personas/Main.java
@@ -1,13 +1,39 @@
 package com.holamundo.personas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Main extends AppCompatActivity {
+    private ListView lv;
+    private String [] opc;
+    private Intent in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        lv = findViewById(R.id.lstOpciones);
+        opc = getResources().getStringArray(R.array.opciones);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, opc);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        in = new Intent(Main.this,Registrar.class);
+                        startActivity(in);
+                        break;
+                    case 1:
+                        in = new Intent(Main.this,Listar.class);
+                        startActivity(in);
+                }
+            }
+        });
     }
 }
